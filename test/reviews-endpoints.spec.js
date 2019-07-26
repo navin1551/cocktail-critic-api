4e9cd41b9f9129addf4a8af59947c99eac082fe5
Navin Kumar <navin1551@gmail.com>
2019-07-26 10:11:11 -0700

    trying to get POST test to work

--- test/reviews-endpoints.spec.js
+++ test/reviews-endpoints.spec.js
@@ -1,5 +1,6 @@
 const app = require("../src/app");
 const knex = require("knex");
+const { makeReviewsArray } = require("./reviews.fixtures");
 
 describe("Review Endpoints", function() {
   let db;
@@ -29,6 +30,51 @@ describe("Review Endpoints", function() {
           .get("/api/reviews")
           .expect(200, []);
       });
+    });
+    context("Given there are reviews in the database", () => {
+      const testReviews = makeReviewsArray();
+
+      beforeEach("insert reviews", () => {
+        return db.into("reviews").insert(testReviews);
+      });
+
+      it("responds with 200 and all of the reviews", () => {
+        return supertest(app)
+          .get("/api/reviews")
+          .expect(200, testReviews);
+      });
+    });
+  });
+
+  describe("POST /api/reviews", () => {
+    it("creates a review, responding with 201 and the new review", () => {
+      const newReview = {
+        name: "Test new name",
+        image: "https://image.com/images.jpeg",
+        comment: "Test comment",
+        rating: 1
+      };
+
+      return supertest(app)
+        .post("/api/reviews")
+        .send(newReview)
+        .expect(201)
+        .expect(res => {
+          expect(res.body.name).to.eql(newReview.name);
+          expect(res.body.image).to.eql(newReview.image);
+          expect(res.body.comment).to.eql(newReview.comment);
+          expect(res.body.rating).to.eql(newReview.rating);
+          expect(res.body).to.have.property("id");
+          expect(res.headers.location).to.eql(`/api/reviews/${res.body.id}`);
+          const expected = new Date().toLocaleString();
+          const actual = new Date(res.body.date_created).toLocaleString();
+          expect(actual).to.eql(expected);
+        })
+        .then(res =>
+          supertest(app)
+            .get(`/api/reviews/${res.body.id}`)
+            .expect(res.body)
+        );
     });
   });
 });
---
+++ test/reviews.fixtures.js
@@ -0,0 +1,46 @@
+function makeReviewsArray() {
+  return [
+    {
+      id: 1,
+      name: "Test name1",
+      image: "https://image.com/images.jpeg",
+      comment: "Comment1",
+      rating: 1,
+      date_created: "2029-01-22T16:28:32.615Z"
+    },
+    {
+      id: 2,
+      name: "Test name2",
+      image: "https://image.com/images.jpeg",
+      comment: "Comment2",
+      rating: 2,
+      date_created: "2029-01-22T16:28:32.615Z"
+    },
+    {
+      id: 3,
+      name: "Test name3",
+      image: "https://image.com/images.jpeg",
+      comment: "Comment3",
+      rating: 3,
+      date_created: "2029-01-22T16:28:32.615Z"
+    },
+    {
+      id: 4,
+      name: "Test name4",
+      image: "https://image.com/images.jpeg",
+      comment: "Comment4",
+      rating: 4,
+      date_created: "2029-01-22T16:28:32.615Z"
+    },
+    {
+      id: 5,
+      name: "Test name5",
+      image: "https://image.com/images.jpeg",
+      comment: "Comment5",
+      rating: 5,
+      date_created: "2029-01-22T16:28:32.615Z"
+    }
+  ];
+}
+
+module.exports = { makeReviewsArray };
