f16d1da763ba946f616489762587f5444550be14
Navin Kumar <navin1551@gmail.com>
2019-07-29 09:30:17 -0700

    deploying db

--- src/config.js
+++ src/config.js
@@ -2,6 +2,10 @@ module.exports = {
   PORT: process.env.PORT || 8000,
   NODE_ENV: process.env.NODE_ENV || "development",
   DB_URL:
-    process.env.DB_URL || "postgresql://postgres@localhost/cocktail-critic",
-  JWT_SECRET: process.env.JWT_SECRET || "change-this-secret"
+    process.env.DATABASE_URL ||
+    "postgresql://postgres@localhost/cocktail-critic",
+  JWT_SECRET: process.env.JWT_SECRET || "change-this-secret",
+  TEST_DB_URL:
+    process.env.TEST_DB_URL ||
+    "postgresql://postgres@localhost/cocktail-critic-test"
 };
